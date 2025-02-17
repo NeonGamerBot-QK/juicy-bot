b4d4085fd953cda798dbf29693d04d0b816711d3
Saahil <neon@saahild.com>
2025-02-16 22:57:36 -0500

    fix:ads

--- src/ads.ts
+++ src/ads.ts
@@ -1,11 +1,11 @@
 // lmao
 export const ads = [
-  `Hello! Have you been looking to learn 3d modelling and CAD Software? Have you been wanting a way to organize your things? If either of those are you, check out the new #printboard ysws! Ship a 3d model that goes with the ikea pegboard, and we will send you one!`,
+  // `Hello! Have you been looking to learn 3d modelling and CAD Software? Have you been wanting a way to organize your things? If either of those are you, check out the new #printboard ysws! Ship a 3d model that goes with the ikea pegboard, and we will send you one!`,
   `#kittycats-cattery, a place where cats roam free`,
   `You should check out #hackpac-drone to watch my club struggle to make a drone that doesn't blow up.`,
   `Join #charmander-island if you want to see development of a virtual farming game im making, and to see a picture of my cats every single day`,
   `Wanna try your hands at jailbreaking a language model? Join #defensive-llm`,
-  `join #skyfalls-airport to see cool stuff being built in public! (slackbots, websites, rust things etc :yay:)`,
+  `join <#C0793T42XV4> to see cool stuff being built in public! (slackbots, websites, rust things etc :yay:)`,
   `Wana see a log of all the kudos? Join #juice-kudos-log`,
   `Wana join an extremly unique _personal channel_ with the one and only zeon? run \`/joinneonschannel\`!`,
 ];
